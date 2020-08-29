bc8f06c043a66e4839967d2030241a8c15177f9f
Luca Bertagna <lbertag@sandia.gov>
2020-08-29 17:30:33 -0600

    Fix kernel_assert test
    Was missing kokkos initialize/finalize.

--- tests/utils/kernel_assert.cpp
+++ tests/utils/kernel_assert.cpp
@@ -2,11 +2,13 @@
 
 #include <Kokkos_Core.hpp>
 
-int main(int,char**) {
+int main(int argc, char** argv) {
+  Kokkos::initialize(argc, argv);
   Kokkos::parallel_for(Kokkos::RangePolicy<>(0,1),
                        KOKKOS_LAMBDA(int) {
     const char* test = "KERNEL_ASSERT_FAIL_TEST";
     EKAT_KERNEL_ASSERT_MSG (1<0,test);
   });
+  Kokkos::finalize();
   return 0;
 }
