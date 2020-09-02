9df221adefc96c81a7dc47b8ce73b5eb2c2924c7
Luca Bertagna <lbertag@sandia.gov>
2020-09-01 22:02:38 -0600

    Fix the need to get kokkos src dir right away
    It is needed to correctly call EkatSetNvccWrapper, which must be done
    *before* the call to 'project'.

--- cmake/Kokkos.cmake
+++ cmake/Kokkos.cmake
@@ -10,20 +10,6 @@ get_property(IS_EKAT_KOKKOS_BUILT GLOBAL PROPERTY EKAT_KOKKOS_BUILT SET)
 # Process the kokkos source directory
 if (NOT IS_EKAT_KOKKOS_BUILT)
 
-  if (NOT Kokkos_SOURCE_DIR)
-    message (STATUS "Kokkos_SOURCE_DIR not specified: using submodule version.")
-    if(Kokkos_ENABLE_DEPRECATED_CODE)
-      message(FATAL_ERROR "Kokkos submodule cannot be used with\n"
-                          "Kokkos_ENABLE_DEPRECATED_CODE.")
-    endif()
-    set (Kokkos_SOURCE_DIR "${PROJECT_SOURCE_DIR}/extern/kokkos" CACHE STRING "Kokkos submodule source directory")
-  elseif (NOT EXISTS ${Kokkos_SOURCE_DIR})
-    message (FATAL_ERROR "Error! Please specify a valid source folder for kokkos.\n"
-                         "       Provided path: ${Kokkos_SOURCE_DIR}")
-  else()
-    message (STATUS "Using Kokkos in ${Kokkos_SOURCE_DIR}.\n"
-                    "User-supplied Kokkos versions are not guaranteed to work.")
-  endif()
   set(Kokkos_BINARY_DIR ${CMAKE_BINARY_DIR}/externals/kokkos)
 
   # Enable Kokkos debug if the host project is in debug mode.
