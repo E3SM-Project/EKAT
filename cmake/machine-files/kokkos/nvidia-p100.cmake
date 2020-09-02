e0fbd8fe9f4373ca20ce37468019fb8ed6427ce5
Luca Bertagna <lbertag@sandia.gov>
2020-09-01 22:05:54 -0600

    More fixes to case-sensitive Kokkos cmake options

--- cmake/machine-files/kokkos/nvidia-p100.cmake
+++ cmake/machine-files/kokkos/nvidia-p100.cmake
@@ -1,4 +1,4 @@
 include (${CMAKE_CURRENT_LIST_DIR}/generic.cmake)
 
 # Enable P100 arch in kokkos
-option(Kokkos_ARCH_Pascal60 "" ON)
+option(Kokkos_ARCH_PASCAL60 "" ON)
--- cmake/machine-files/kokkos/nvidia-v100.cmake
+++ cmake/machine-files/kokkos/nvidia-v100.cmake
@@ -1,4 +1,4 @@
 include (${CMAKE_CURRENT_LIST_DIR}/generic.cmake)
 
 # Enable V100 arch in kokkos
-option(Kokkos_ARCH_Volta70 "" ON)
+option(Kokkos_ARCH_VOLTA70 "" ON)
