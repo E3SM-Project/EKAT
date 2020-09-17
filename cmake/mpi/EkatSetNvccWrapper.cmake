00526dd492e5fdd32564b2af775c377cf9e80105
Luca Bertagna <lbertag@sandia.gov>
2020-09-17 11:10:05 -0600

    Fix path error in EkatSetNvccWrapper

--- cmake/mpi/EkatSetNvccWrapper.cmake
+++ cmake/mpi/EkatSetNvccWrapper.cmake
@@ -1,7 +1,7 @@
 # Note: We cannot use 'EKAT_SOURCE_DIR', since one may be calling this macro
 #       before processing the ekat subfolder. We have to compute the location
 #       of ekat_mpicxx.in relative to this file.
-set (MPICXX_WRAPPER_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/../bin)
+set (MPICXX_WRAPPER_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/../../bin)
 macro (EkatSetNvccWrapper)
   # Check if ekat_mpicxx is already the CMAKE_CXX_COMPILER. This could happen if
   # one configures the project, then changes something that triggers cmake to run.
