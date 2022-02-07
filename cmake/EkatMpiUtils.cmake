72f6292c5c80d8bdca0e5b1c0ffd5ed8d52714eb
Luca Bertagna <lbertag@sandia.gov>
2022-02-07 15:35:30 -0700

    Changed how EKAT handles MPI in order to build
    Instead of using MPI compiler wrappers, we use native
    C++/Fortran compilers, and link against MPI libs
    (which is what the wrapper do for you in the background).
    
    This allows to simplify and/or remove some cmake logic
    in EKAT. Furthermore, it will easily allow, in a following
    PR, to turn MPI into an optional dependency.

--- cmake/EkatMpiUtils.cmake
+++ cmake/EkatMpiUtils.cmake
@@ -1,24 +1,20 @@
 # Detect the library that provides MPI
 set (EKAT_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR})
 macro (GetMpiDistributionName DISTRO_NAME)
-  execute_process(COMMAND ${CMAKE_CXX_COMPILER} 
-                  ${EKAT_CMAKE_DIR}/TryCompileOpenMPI.cpp
-                  OUTPUT_QUIET ERROR_QUIET
-                  RESULT_VARIABLE HAVE_OPENMPI_MPI)
+  execute_process(COMMAND ${MPIEXEC_EXECUTABLE} --version
+                  OUTPUT_VARIABLE OUT_VAR
+                  RESULT_VARIABLE SUCCESS)
+  if (NOT SUCCESS EQUAL 0)
+    message (FATAL_ERROR
+      "Error! Could not run '${MPIEXEC_EXECUTABLE} --version'")
+  endif()
 
-  if (HAVE_OPENMPI_MPI)
+  if (OUT_VAR MATCHES "open-mpi|OpenMPI|Open MPI|OpenRTE")
     set (${DISTRO_NAME} "openmpi")
+  elseif (OUT_VAR MATCHES "mpich|MPICH")
+    set (${DISTRO_NAME} "mpich")
   else ()
-    execute_process(COMMAND ${CMAKE_CXX_COMPILER} 
-                    ${CMAKE_CURRENT_SOURCE_DIR}/cmake/TryCompileMPICH.cpp
-                    OUTPUT_QUIET ERROR_QUIET
-                    RESULT_VARIABLE HAVE_MPICH_MPI)
-
-    if (HAVE_MPICH_MPI)
-      set (${DISTRO_NAME} "mpich")
-    else ()
-      set (${DISTRO_NAME} "unknown")
-    endif()
+    set (${DISTRO_NAME} "unknown")
   endif()
 endmacro ()
 
@@ -59,19 +55,4 @@ macro (SetMpiRuntimeEnvVars)
   endif()
 
   unset (DISTRO_NAME)
-endmacro()
-
-# Set the MPI cxx backend compiler var name
-macro (SetMpiCxxBackendCompilerVarName OUTPUT_VAR_NAME)
-  set (DISTRO_NAME)
-  GetMpiDistributionName (DISTRO_NAME)
-
-  if ("${DISTRO_NAME}" STREQUAL "openmpi")
-    set (${OUTPUT_VAR_NAME} OMPI_CXX)
-  elseif ("${DISTRO_NAME}" STREQUAL "mpich")
-    set (${OUTPUT_VAR_NAME} MPICH_CXX)
-  else ()
-    message (FATAL_ERROR "Unsupported MPI distribution.")
-  endif()
-  unset(DISTRO_NAME)
 endmacro()
