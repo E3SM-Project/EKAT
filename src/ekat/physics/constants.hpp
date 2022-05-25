d493c25bffef802bf39fa33f233f438f086b4103
Peter Bosler <pabosle@sandia.gov>
2022-05-25 13:18:47 -0600

    updated to include all shr_consts and to include a .gitignore for the auto-generated source files.

--- src/ekat/physics/constants.hpp
+++ src/ekat/physics/constants.hpp
@@ -26,8 +26,8 @@ namespace physics {
    to enable the CMake parser to use the same numeric values for the
    auto-generated .cpp file and Fortran constants module.
 
-  All constants must be placed between the lines containing
-  EKAT_CONSTANTS_START_PARSE and EKAT_CONSTANTS_END_PARSE.
+  All constants must be placed between the lines containing the uppercase strings
+  "ekat_constants_start_parse" and "ekat_constants_end_parse".
  */
 
 template <typename Scalar>
@@ -41,8 +41,6 @@ struct Constants
   /// EKAT_CONSTANTS_START_PARSE
   /// pi
   static constexpr Scalar pi = 3.14159265358979323846264;
-  /// 1/6th of the pi
-  static constexpr Scalar pi_sixth = pi/6;
   /// Avogadro's constant [#/mol]
   static constexpr Scalar avogadro = 6.022214076e23;
   /// Boltzmann's constant [J/K]
@@ -53,8 +51,10 @@ struct Constants
   static constexpr Scalar gravity = 9.80616;
   /// radius of a spherical Earth [m]
   static constexpr Scalar earth_radius = 6.37122e6;
-  /// standard pressure [Pa]
+  /// standard pressure [Pa] (1 atm, pre-1982 definition)
   static constexpr Scalar std_pressure = 101325.0;
+  /// standard pressure [Pa] (1 bar, post-1982 definition)
+  static constexpr Scalar standard_pressure = 100000.0;
   /// sidereal day [s]
   static constexpr Scalar sidereal_day = 86164;
   /// calendar day [s]
@@ -63,38 +63,46 @@ struct Constants
   static constexpr Scalar earth_omega = 2*pi/sidereal_day;
   /// stefan-boltzmann constant [W/(m2 K4)]
   static constexpr Scalar stefan_boltzmann = 5.67e-8;
+  /// Karman constant
+  static constexpr Scalar karman = 0.4;
 
   /**
 
     Chemistry constants
 
   */
-  /// Molecular weight of water [kg/mol]
-  static constexpr Scalar molec_weight_h2o = 0.018016;
-  /// Molecular weight of dry air [kg/mol]
-  static constexpr Scalar molec_weight_dry_air = 0.028966;
-  /// Molecular weight of sulfuric acid @f$\text{H}_2\text{SO}_4@f$ [kg/mol]
-  static constexpr Scalar molec_weight_h2so4 = 0.098079;
-  /// Molecular weight of ammonia @f$\text{N}\text{H}_3@f$ [kg/mol]
-  static constexpr Scalar molec_weight_nh3 = 0.01703052;
-  /// Molecular weight of sulfate ion @f$\text{SO}_4^{2-}@f$ [kg/mol]
-  static constexpr Scalar molec_weight_so4 = 0.09606;
-  /// Molecular weight of ammonium ion @f$\text{NH}_4^+@f$ [kg/mol]
-  static constexpr Scalar molec_weight_nh4 = 0.018039;
-  /// Molecular weight of carbon dioxide @f$\text{CO}_2@f$ [kg/mol]
-  static constexpr Scalar molec_weight_co2 = 0.0440095;
-  /// Molecular weight of ozone @f$\text{O}_3@f$ [kg/mol]
-  static constexpr Scalar molec_weight_o3 = 0.0479982;
-  /// Molecular weight of nitrous oxide @f$\text{NO}_2@f$ [kg/mol]
-  static constexpr Scalar molec_weight_no2 = 0.0440128;
-  /// Molecular weight of carbon monoxide @f$\text{CO}@f$ [kg/mol]
-  static constexpr Scalar molec_weight_co = 0.0280101;
+  /// Molecular weight of carbon [kg/mol]
+  static constexpr Scalar molec_weight_c = 0.0120107;
   /// Molecular weight of methane @f$\text{CH}_4@f$ [kg/mol]
   static constexpr Scalar molec_weight_ch4 = 0.01604246;
-  /// Molecular weight of oxygen @f$\text{O}_2@f$ [kg/mol]
-  static constexpr Scalar molec_weight_o2 = 0.031998;
+  /// Molecular weight of carbon monoxide @f$\text{CO}@f$ [kg/mol]
+  static constexpr Scalar molec_weight_co = 0.0280101;
+  /// Molecular weight of carbon dioxide @f$\text{CO}_2@f$ [kg/mol]
+  static constexpr Scalar molec_weight_co2 = 0.0440095;
+  /// Molecular weight of dry air [kg/mol]
+  static constexpr Scalar molec_weight_dry_air = 0.028966;
+  /// Molecular weight of water [kg/mol]
+  static constexpr Scalar molec_weight_h2o = 0.018016;
+  /// Molecular weight of sulfuric acid @f$\text{H}_2\text{SO}_4@f$ [kg/mol]
+  static constexpr Scalar molec_weight_h2so4 = 0.098079;
   /// Molecular weight of nitrogen @f$\text{N}_2@f$ [kg/mol]
   static constexpr Scalar molec_weight_n2 = 0.0280134;
+  /// Molecular weight of ammonia @f$\text{N}\text{H}_3@f$ [kg/mol]
+  static constexpr Scalar molec_weight_nh3 = 0.01703052;
+  /// Molecular weight of ammonium ion @f$\text{NH}_4^+@f$ [kg/mol]
+  static constexpr Scalar molec_weight_nh4 = 0.018039;
+  /// Molecular weight of nitrous oxide @f$\text{NO}_2@f$ [kg/mol]
+  static constexpr Scalar molec_weight_no2 = 0.0440128;
+  /// Molecular weight of oxygen atom [kg/mol]
+  static constexpr Scalar molec_weight_o = 0.0159994;
+  /// Molecular weight of oxygen @f$\text{O}_2@f$ [kg/mol]
+  static constexpr Scalar molec_weight_o2 = 0.031998;
+  /// Molecular weight of ozone @f$\text{O}_3@f$ [kg/mol]
+  static constexpr Scalar molec_weight_o3 = 0.0479982;
+  /// Molecular weight of sulfate ion @f$\text{SO}_4^{2-}@f$ [kg/mol]
+  static constexpr Scalar molec_weight_so4 = 0.09606;
+  /// ratio of carbon isotopes in Pee Dee Belemnite standard
+  static constexpr Scalar carbon_13_to_12_ratio = 0.0112372;
 
   /**
 
@@ -102,7 +110,7 @@ struct Constants
 
   */
   /// Ratio of molecular weights of water and dry air (often denoted \epsilon)
-  static constexpr Scalar weight_ratio_h20_air = molec_weight_h2o / molec_weight_dry_air;
+  static constexpr Scalar weight_ratio_h2o_air = molec_weight_h2o / molec_weight_dry_air;
   /// density of freshwater
   static constexpr Scalar density_h2o_liquid = 1e3;
   /// density of freshwater ice
@@ -115,6 +123,8 @@ struct Constants
   static constexpr Scalar latent_heat_evap = 2.501e6;
   /// latent heat
   static constexpr Scalar latent_heat_fusion = 3.337e5;
+  /// latent heat
+  static constexpr Scalar latent_heat_sublimation = latent_heat_fusion + latent_heat_evap;
   /// gas constant for water vapor
   static constexpr Scalar rgas_h2o_vapor = r_gas / molec_weight_h2o;
   /// flat surface tension at freeze point
@@ -127,6 +137,51 @@ struct Constants
   static constexpr Scalar cp_h2o_ice = 2117.27;
   /// triple point temperature [K]
   static constexpr Scalar triple_point_h2o = 273.16;
+  /**
+    seawater / ocean
+  */
+  /// density of seawater
+  static constexpr Scalar density_seawater = 1026;
+  /// freeze/melt point
+  static constexpr Scalar freezing_pt_seawater = freezing_pt_h2o - 1.8;
+  /// ocean surface layer depth [m] for diurnal SST
+  static constexpr Scalar ocean_surface_layer_depth = 3;
+  /// ocean reference salinity (psu)
+  static constexpr Scalar ocean_ref_salinity = 34.7;
+  /// specific heat of seawater
+  static constexpr Scalar cp_seawater = 3996;
+  /**
+    vmsow : Vienna mean standard ocean water
+  */
+  /// ratio of O18 to O16
+  static constexpr Scalar vmsow_ratio_o18_o16 = 2005.2e-6;
+  /// ratio of O17 to O16
+  static constexpr Scalar vmsow_ratio_o17_o16 = 379e-6;
+  /// ratio of O16 to total oxygen
+  static constexpr Scalar vmsow_ratio_o16_total_o = 0.997628;
+  /// ratio of deuterium to hydrogen
+  static constexpr Scalar vmsow_ratio_2h_h = 155.76e-6;
+  /// ratio of tritium to hydrogen
+  static constexpr Scalar vmsow_ratio_3h_h = 1.85e-6;
+  /// ratio of hydogren to total hydrogen
+  static constexpr Scalar vmsow_ratio_h_total_h = 0.99984426;
+
+
+  /**
+    ice
+  */
+  /// ice reference salinity [psu]
+  static constexpr Scalar ice_ref_salinity = 4;
+  /// freezing point at zero pressure in an sub-shelf ice cavity
+  static constexpr Scalar freezing_pt_h2o_zerop = 273.2122;
+  /// linear coefficient sub-shelf freezing pt change vs pressure [K/Pa]
+  static constexpr Scalar dfreeze_zerop_dp = -7.43e-8;
+  /// linear coefficient sub-shelf freezing pt change vs salinity [K/psu]
+  static constexpr Scalar dfreeze_zerop_ds = -5.63e-2;
+  /// thermal conductivity of land ice [W/m/K]
+  static constexpr Scalar thermal_conductivity_ice = 2.1;
+  /// thermal diffusivity of land ice (denoted kappa)
+  static constexpr Scalar thermal_diffusivity_landice = thermal_conductivity_ice/(density_h2o_ice*cp_h2o_ice);
 
   /**
 
@@ -137,6 +192,10 @@ struct Constants
   static constexpr Scalar rgas_dry_air = r_gas / molec_weight_dry_air;
   /// specific heat at constant pressure for dry air
   static constexpr Scalar cp_dry_air = 1004.64;
+  /// Ratio of water vapor and dry air gas constants, minus one (denoted zvir in e3sm)
+  static constexpr Scalar gas_const_ratio_zvir = rgas_h2o_vapor/rgas_dry_air - 1;
+  /// Ratio of water vapor and dry air specific heats, minus one (denoted cpvir in e3sm)
+  static constexpr Scalar specific_heat_ratio_cpvir = cp_h2o_vapor/cp_dry_air - 1;
 
   /// EKAT_CONSTANTS_END_PARSE
 };
--- tests/physics/constants_tests.cpp
+++ tests/physics/constants_tests.cpp
@@ -10,47 +10,82 @@ TEST_CASE("physics_constants", "[physics]") {
   using constants = Constants<double>;
 
   REQUIRE(4*std::atan(1) == Approx(constants::pi));
-  REQUIRE(6*constants::pi_sixth == Approx(constants::pi));
   REQUIRE(constants::avogadro == Approx(6.022e23).epsilon(0.01));
+  REQUIRE(constants::boltzmann == Approx(1.38e-23).epsilon(0.01));
   REQUIRE(constants::r_gas == Approx(8.31446).epsilon(0.01));
   REQUIRE(constants::gravity == 9.80616);
   REQUIRE(constants::earth_radius == 6.37122e6);
   REQUIRE(constants::std_pressure == 101325);
+  REQUIRE(constants::standard_pressure == 100000);
   REQUIRE(constants::sidereal_day == 86164);
   REQUIRE(constants::calendar_day == 24*60*60);
   REQUIRE(constants::earth_omega * constants::sidereal_day == Approx(2*constants::pi));
   REQUIRE(constants::stefan_boltzmann == 5.67e-8);
+  REQUIRE(constants::karman == 0.4);
 
-  const double standard_pressure = 100000; // Pa
   const auto standard_temperature = constants::freezing_pt_h2o;
   const auto rdry = constants::rgas_dry_air;
-  const auto rho = standard_pressure / (rdry * standard_temperature);
+  const auto rho = constants::standard_pressure / (rdry * standard_temperature);
   const auto P = rho*rdry*constants::melting_pt_h2o;
 
-  REQUIRE( P == Approx(standard_pressure));
+  REQUIRE( P == Approx(constants::standard_pressure));
 
+  REQUIRE(constants::molec_weight_c * 1000 == Approx(12.0107));
+  REQUIRE(constants::molec_weight_ch4 * 1000 == Approx(16.04).epsilon(0.001));
+  REQUIRE(constants::molec_weight_co * 1000 == Approx(28.01));
+  REQUIRE(constants::molec_weight_co2 * 1000 == Approx(44.01));
   REQUIRE(constants::molec_weight_dry_air * 1000 == Approx(28.966));
   REQUIRE(constants::molec_weight_h2o * 1000 == Approx(18.016));
   REQUIRE(constants::molec_weight_h2so4 * 1000 == Approx(98.079));
-  REQUIRE(constants::molec_weight_nh3 * 1000 == Approx(17.031).epsilon(0.01));
-  REQUIRE(constants::molec_weight_so4 * 1000 == Approx(96.06));
+  REQUIRE(constants::molec_weight_n2 * 1000 == Approx(28.0134));
+  REQUIRE(constants::molec_weight_nh3 * 1000 == Approx(17.03052));
   REQUIRE(constants::molec_weight_nh4 * 1000 == Approx(18.04).epsilon(0.01));
-  REQUIRE(constants::molec_weight_co2 * 1000 == Approx(44.01));
-  REQUIRE(constants::molec_weight_o3 * 1000 == Approx(48).epsilon(0.01));
   REQUIRE(constants::molec_weight_no2 * 1000 == Approx(44.013));
-  REQUIRE(constants::molec_weight_co * 1000 == Approx(28.01));
-  REQUIRE(constants::molec_weight_ch4 * 1000 == Approx(16.04).epsilon(0.001));
-  REQUIRE(constants::molec_weight_o2 * 1000 == Approx(32).epsilon(0.001));
-  REQUIRE(constants::molec_weight_n2 * 1000 == Approx(28).epsilon(0.001));
+  REQUIRE(constants::molec_weight_o * 1000 == Approx(15.9994));
+  REQUIRE(constants::molec_weight_o2 * 1000 == Approx(31.998));
+  REQUIRE(constants::molec_weight_o3 * 1000 == Approx(47.9982));
+  REQUIRE(constants::molec_weight_so4 * 1000 == Approx(96.06));
+  REQUIRE(constants::carbon_13_to_12_ratio == 0.0112372);
 
-  REQUIRE(constants::triple_point_h2o == 273.16);
+  REQUIRE(constants::weight_ratio_h2o_air == Approx(0.62197).epsilon(0.00001));
   REQUIRE(constants::density_h2o_liquid == 1000);
   REQUIRE(constants::density_h2o_ice == 917);
-  REQUIRE(constants::cp_dry_air == 1.00464e3);
-  REQUIRE(constants::cp_h2o_vapor == 1.81e3);
-  REQUIRE(constants::cp_h2o_liq == 4.188e3);
-  REQUIRE(constants::cp_h2o_ice == 2.11727e3);
+  REQUIRE(constants::freezing_pt_h2o == 273.15);
+  REQUIRE(constants::melting_pt_h2o == 273.15);
   REQUIRE(constants::latent_heat_evap == 2.501e6);
   REQUIRE(constants::latent_heat_fusion == 3.337e5);
+  REQUIRE(constants::latent_heat_sublimation == 2834700);
+  REQUIRE(constants::rgas_h2o_vapor == Approx(461.501).epsilon(0.001));
+  REQUIRE(constants::surface_tension_h2o_air_273k == 0.07564);
+  REQUIRE(constants::cp_h2o_liq == 4.188e3);
+  REQUIRE(constants::cp_h2o_vapor == 1.81e3);
+  REQUIRE(constants::cp_h2o_ice == 2.11727e3);
+  REQUIRE(constants::cp_dry_air == 1.00464e3);
+  REQUIRE(constants::triple_point_h2o == 273.16);
 
+  REQUIRE(constants::density_seawater == 1026);
+  REQUIRE(constants::freezing_pt_seawater == Approx(271.35));
+  REQUIRE(constants::ocean_surface_layer_depth == 3);
+  REQUIRE(constants::ocean_ref_salinity == 34.7);
+  REQUIRE(constants::cp_seawater == 3996);
+
+  REQUIRE(constants::vmsow_ratio_o18_o16 == 2005.2e-6);
+  REQUIRE(constants::vmsow_ratio_o17_o16 == 379e-6);
+  REQUIRE(constants::vmsow_ratio_o16_total_o == 0.997628);
+  REQUIRE(constants::vmsow_ratio_2h_h == 155.76e-6);
+  REQUIRE(constants::vmsow_ratio_3h_h == 1.85e-6);
+  REQUIRE(constants::vmsow_ratio_h_total_h == 0.99984426);
+
+  REQUIRE(constants::ice_ref_salinity == 4);
+  REQUIRE(constants::freezing_pt_h2o_zerop == 273.2122);
+  REQUIRE(constants::dfreeze_zerop_dp == -7.43e-8);
+  REQUIRE(constants::dfreeze_zerop_ds == -5.63e-2);
+  REQUIRE(constants::thermal_conductivity_ice == 2.1);
+
+  REQUIRE(constants::rgas_dry_air == Approx(287.0420493).epsilon(0.001));
+  REQUIRE(constants::cp_dry_air == 1004.64);
+  REQUIRE(constants::gas_const_ratio_zvir ==
+    constants::rgas_h2o_vapor/constants::rgas_dry_air - 1);
+  REQUIRE(constants::specific_heat_ratio_cpvir ==
+    constants::cp_h2o_vapor/constants::cp_dry_air -1);
 }
