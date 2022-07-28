7c85c08507f12fe0d0ae350c2d4fbd18c37fc211
Oksana Guba <oksanaguba@gmail.com>
2022-07-28 18:54:15 -0400

    add crusher files to ekat kokkos cmake

---
+++ cmake/machine-files/kokkos/hip.cmake
@@ -0,0 +1,5 @@
+# Settings used when Cuda is the Kokkos backend
+
+#i actually do not know if this is needed
+set(Kokkos_ENABLE_AGGRESSIVE_VECTORIZATION FALSE CACHE BOOL "")
+set(Kokkos_ENABLE_HIP TRUE CACHE BOOL "")
