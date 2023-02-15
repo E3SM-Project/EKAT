f301e6e3e7f3f18287a08c704746d1ab345c7fbd
James Foucar <jgfouca@sandia.gov>
2023-02-15 13:59:40 -0700

    Another fix. SCREAM_MACHINE needs to be in env for impl

--- scripts/jenkins/jenkins.sh
+++ scripts/jenkins/jenkins.sh
@@ -17,6 +17,7 @@ export SCREAM_SCRIPTS=${WORK_DIR}/scream/components/eamxx/scripts
 export JENKINS_SCRIPT_DIR=${SCREAM_SCRIPTS}/jenkins # some scream env setups depend on this
 source ${SCREAM_SCRIPTS}/jenkins/${NODE_NAME}_setup
 source ${SCREAM_SCRIPTS}/source_to_load_scream_env.sh
+export SCREAM_MACHINE
 
 BATCHP=$(${SCREAM_SCRIPTS}/query-scream $SCREAM_MACHINE batch)
 
