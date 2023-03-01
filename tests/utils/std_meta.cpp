db44099aa170096bdb1e34bb891ea49170122b0d
Luca Bertagna <lbertag@sandia.gov>
2023-03-01 14:31:04 -0700

    Fix clashes with stk names in std_meta test

--- tests/utils/std_meta.cpp
+++ tests/utils/std_meta.cpp
@@ -5,22 +5,21 @@
 #include <vector>
 
 TEST_CASE ("any") {
-  using namespace ekat;
 
-  any a,b;
+  ekat::any a,b;
 
   std::vector<int> u = {1,2};
 
   a.reset<std::vector<int>>(u);
   b.reset(u);
 
-  REQUIRE (any_cast<std::vector<int>>(a)==any_cast<std::vector<int>>(b));
-  REQUIRE (any_cast<std::vector<int>>(a)==u);
-  REQUIRE_THROWS (any_cast<std::vector<double>>(a));
+  REQUIRE (ekat::any_cast<std::vector<int>>(a)==ekat::any_cast<std::vector<int>>(b));
+  REQUIRE (ekat::any_cast<std::vector<int>>(a)==u);
+  REQUIRE_THROWS (ekat::any_cast<std::vector<double>>(a));
 
   REQUIRE (a.isType<std::vector<int>>());
   REQUIRE (not a.isType<int>());
 
-  any c (u);
-  REQUIRE (any_cast<std::vector<int>>(a)==any_cast<std::vector<int>>(c));
+  ekat::any c (u);
+  REQUIRE (ekat::any_cast<std::vector<int>>(a)==ekat::any_cast<std::vector<int>>(c));
 }
