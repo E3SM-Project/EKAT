607983b8c3d5ccb4827cc4448ab4f02db66c309b
Luca Bertagna <lbertag@sandia.gov>
2023-03-01 13:23:05 -0700

    Add small unit test for ekat::any

---
+++ tests/utils/std_meta.cpp
@@ -0,0 +1,23 @@
+#include <catch2/catch.hpp>
+
+#include "ekat/std_meta/ekat_std_any.hpp"
+
+#include <vector>
+
+TEST_CASE ("any") {
+  using namespace ekat;
+
+  any a,b;
+
+  std::vector<int> u = {1,2};
+
+  a.reset<std::vector<int>>(u);
+  b.reset(u);
+
+  REQUIRE (any_cast<std::vector<int>>(a)==any_cast<std::vector<int>>(b));
+  REQUIRE (any_cast<std::vector<int>>(a)==u);
+  REQUIRE_THROWS (any_cast<std::vector<double>>(a));
+
+  REQUIRE (a.isType<std::vector<int>>());
+  REQUIRE (not a.isType<int>());
+}
