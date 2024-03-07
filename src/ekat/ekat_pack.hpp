19df29793b77e30c1ad3d7d692c8356401b6dfb4
Luca Bertagna <lbertag@sandia.gov>
2024-03-06 23:18:44 -0700

    Add missing specialization of IsPack meta-utility

--- src/ekat/ekat_pack.hpp
+++ src/ekat/ekat_pack.hpp
@@ -366,7 +366,8 @@ template<typename T>
 struct IsPack : std::false_type {};
 template<typename T, int N>
 struct IsPack<Pack<T,N>> : std::true_type {};
-
+template<typename T, int N>
+struct IsPack<const Pack<T,N>> : std::true_type {};
 
 // Later, we might support type promotion. For now, caller must explicitly
 // promote a pack's scalar type in mixed-type arithmetic.
