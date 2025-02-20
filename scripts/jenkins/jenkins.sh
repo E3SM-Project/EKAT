12057410f6b9e32fe3c12b2eaf7203e488da0d86
James Foucar <jgfouca@sandia.gov>
2025-02-20 14:31:03 -0700

    De-scream a few things in EKAT

--- scripts/jenkins/jenkins.sh
+++ scripts/jenkins/jenkins.sh
@@ -16,7 +16,7 @@ export WORK_DIR=$(pwd)
 export SCREAM_SCRIPTS=${WORK_DIR}/scream/components/eamxx/scripts
 export JENKINS_SCRIPT_DIR=${SCREAM_SCRIPTS}/jenkins # some scream env setups depend on this
 source ${SCREAM_SCRIPTS}/jenkins/${NODE_NAME}_setup
-source ${SCREAM_SCRIPTS}/source_to_load_scream_env.sh
+source ${SCREAM_SCRIPTS}/source_to_load_eamxx_env.sh
 
 if [ -z "$SCREAM_MACHINE" ]; then
     echo "SCREAM_MACHINE must be set by ${SCREAM_SCRIPTS}/jenkins/${NODE_NAME}_setup in order for jenkins infrastructure to work"
@@ -25,7 +25,7 @@ fi
 
 export SCREAM_MACHINE
 
-BATCHP=$(${SCREAM_SCRIPTS}/query-scream $SCREAM_MACHINE batch)
+BATCHP=$(${SCREAM_SCRIPTS}/query-eamxx $SCREAM_MACHINE batch)
 
 set -o pipefail
 $BATCHP $EKAT_JENKINS_SCRIPT_DIR/jenkins_impl.sh 2>&1 | tee JENKINS_$DATE_STAMP
