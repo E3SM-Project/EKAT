b11a86d467f16ec7d57564640094bd2d45ecadcc
James Foucar <jgfouca@sandia.gov>
2025-04-15 10:09:07 -0600

    Update blake cmake settings

--- cmake/machine-files/blake.cmake
+++ cmake/machine-files/blake.cmake
@@ -1,7 +1,3 @@
-# Load skx arch and openmp backend for kokkos
-include (${CMAKE_CURRENT_LIST_DIR}/kokkos/intel-skx.cmake)
-include (${CMAKE_CURRENT_LIST_DIR}/kokkos/openmp.cmake)
-
-# Blake cannot use srun for some reason and therefore needs EKAT
-# to manage resources.
-set (EKAT_TEST_LAUNCHER_MANAGE_RESOURCES True CACHE BOOL "")
+# Load H100/HOPPER arch and cuda backend for kokkos
+include (${CMAKE_CURRENT_LIST_DIR}/kokkos/nvidia-h100.cmake)
+include (${CMAKE_CURRENT_LIST_DIR}/kokkos/cuda.cmake)
