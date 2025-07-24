5987c28add443e5d28a3e507ee2e9fd2ac03b4df
Luca Bertagna <lbertag@sandia.gov>
2025-07-24 14:37:18 -0600

    Add ghci-snl* machine files

---
+++ cmake/machine-files/ghci-snl-cpu.cmake
@@ -0,0 +1,5 @@
+# Common settings for our ghci images
+include(${CMAKE_CURRENT_LIST_DIR}/ghci-snl.cmake)
+
+# Currently, we have 32 cores for each ghci-snl-cpu instance, but 4 ranks is enough
+set(SCREAM_TEST_MAX_RANKS 4 CACHE STRING "Upper limit on ranks for mpi tests")
