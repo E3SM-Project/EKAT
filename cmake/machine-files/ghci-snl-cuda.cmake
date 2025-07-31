3d6244a48ddb21624452ada545c041f8e9aee4e0
Luca Bertagna <lbertag@sandia.gov>
2025-07-31 17:00:51 -0600

    Fix mach files options copied from eamxx repo

--- cmake/machine-files/ghci-snl-cuda.cmake
+++ cmake/machine-files/ghci-snl-cuda.cmake
@@ -7,4 +7,4 @@ include (${CMAKE_CURRENT_LIST_DIR}/kokkos/cuda.cmake)
 set(EKAT_MPI_NP_FLAG "-n" CACHE STRING "The mpirun flag for designating the total number of ranks")
 
 # Currently, we have 2 GPUs/node on Blake, and we run a SINGLE build per node, so we can fit 2 ranks there
-set(SCREAM_TEST_MAX_RANKS 2 CACHE STRING "Upper limit on ranks for mpi tests")
+set(EKAT_TEST_MAX_RANKS 2 CACHE STRING "Upper limit on ranks for mpi tests")
