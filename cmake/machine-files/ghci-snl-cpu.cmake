952211d1af7b4e602bf3c3645062ab102acec49e
Luca Bertagna <lbertag@sandia.gov>
2025-07-31 17:00:51 -0600

    Fix mach files options copied from eamxx repo

--- cmake/machine-files/ghci-snl-cpu.cmake
+++ cmake/machine-files/ghci-snl-cpu.cmake
@@ -2,4 +2,4 @@
 include(${CMAKE_CURRENT_LIST_DIR}/ghci-snl.cmake)
 
 # Currently, we have 32 cores for each ghci-snl-cpu instance, but 4 ranks is enough
-set(SCREAM_TEST_MAX_RANKS 4 CACHE STRING "Upper limit on ranks for mpi tests")
+set(EKAT_TEST_MAX_RANKS 4 CACHE STRING "Upper limit on ranks for mpi tests")
