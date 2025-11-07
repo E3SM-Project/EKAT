fbc463c264ea31a66130a73956859933e3796116
Luca Bertagna <lbertag@sandia.gov>
2025-11-06 19:04:44 -0700

    Provide single entry point header for all expression types

---
+++ src/expression/ekat_expression.hpp
@@ -0,0 +1,15 @@
+#ifndef EKAT_EXPRESSION_HPP
+#define EKAT_EXPRESSION_HPP
+
+// For ease of use, include all expression classes headers in one header
+// This way, users can just write down the expression, without bothering
+// about including the correct headers that define the particular operation
+
+#include "ekat_expression_assignment_op.hpp"
+#include "ekat_expression_binary_op.hpp"
+#include "ekat_expression_compare.hpp"
+#include "ekat_expression_conditional.hpp"
+#include "ekat_expression_math.hpp"
+#include "ekat_expression_view.hpp"
+
+#endif // EKAT_EXPRESSION_HPP
