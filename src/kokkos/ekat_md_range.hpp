50c025f95b1ab00ca84438f1cb3542e1d9bfbd93
Luca Bertagna <lbertag@sandia.gov>
2026-02-02 10:29:32 -0700

    Support multi-arg lambdas also with TeamVectorRange for loops

--- src/kokkos/ekat_md_range.hpp
+++ src/kokkos/ekat_md_range.hpp
@@ -3,17 +3,18 @@
 
 namespace ekat {
 
+// Outermost kernel
 template<typename MDPolicy, typename Functor>
 void parallel_for (const MDPolicy& p, const Functor& f)
 {
   using exec_space = typename MDPolicy::traits::execution_space;
-  constexpr int N = MDPolicy::rank;
-  if constexpr (rank==1) {
+  constexpr int N = MDPolicy::N;
+  if constexpr (N==1) {
     Kokkos::RangePolicy<exec_space> p(p.m_lower[0],p.m_upper[0]),
     Kokkos::parallel_for(p, KOKKOS_LAMBDA(int i) {
       f(i);
     }):
-  } else if constexpr (rank==2) {
+  } else if constexpr (N==2) {
     int low0 = p.m_lower[0];
     int low1 = p.m_lower[1];
     int dim0 = p.m_upper[0] - low0;
@@ -26,7 +27,7 @@ void parallel_for (const MDPolicy& p, const Functor& f)
 
       f(i,j);
     }):
-  } else if constexpr (rank==3) {
+  } else if constexpr (N==3) {
     int low0 = p.m_lower[0];
     int low1 = p.m_lower[1];
     int low2 = p.m_lower[2];
@@ -42,7 +43,7 @@ void parallel_for (const MDPolicy& p, const Functor& f)
 
       f(i,j,k);
     }):
-  } else if constexpr (rank==4) {
+  } else if constexpr (N==4) {
     int low0 = p.m_lower[0];
     int low1 = p.m_lower[1];
     int low2 = p.m_lower[2];
@@ -62,10 +63,88 @@ void parallel_for (const MDPolicy& p, const Functor& f)
       f(i,j,k,l);
     }):
   } else {
-    EKAT_ERROR_MSG ("Error! Unsupported MDRange rank (" + std::to_string(N) + ").\n");
+    EKAT_ERROR_MSG ("Error! Unsupported MDRange N (" + std::to_string(N) + ").\n");
   }
 }
 
+// Team-vector range
+template<typename TeamMember, int N, typename Functor>
+KOKKOS_INLINE_FUNCTION
+void md_team_vector_range_for (const TeamMember& t, int lower[N], int upper[N], const Functor& f)
+{
+  if constexpr (N==1) {
+    int dim0 = upper[0] - lower[0];
+    Kokkos::parallel_for(Kokkos::TeamVectorRange(t,dim0),[&](int i) {
+      f(lower[0] + i);
+    });
+  } else if constexpr (N==2) {
+    int dim0 = upper[0] - lower[0];
+    int dim1 = upper[1] - lower[1];
+    Kokkos::parallel_for(Kokkos::TeamVectorRange(t,dim0*dim1),[&](int idx) {
+      int i = lower[0] + idx / dim1;
+      int j = lower[1] + idx % dim1;
+      f(i,j);
+    });
+  } else if constexpr (N==3) {
+    int dim0 = upper[0] - lower[0];
+    int dim1 = upper[1] - lower[1];
+    int dim2 = upper[2] - lower[2];
+    Kokkos::parallel_for(Kokkos::TeamVectorRange(t,dim0*dim1*dim2),[&](int idx) {
+      int i = lower[0] + (idx / dim2) / dim1;
+      int j = lower[1] + (idx / dim2) % dim1;
+      int k = lower[2] +  idx % dim2;
+      f(i,j,k);
+    });
+  } else if constexpr (N==4) {
+    int dim0 = upper[0] - lower[0];
+    int dim1 = upper[1] - lower[1];
+    int dim2 = upper[2] - lower[2];
+    int dim3 = upper[3] - lower[3];
+    Kokkos::parallel_for(Kokkos::TeamVectorRange(t,dim0*dim1*dim2*dim3),[&](int idx) {
+      int i = lower[0] + ((idx / dim3) / dim2) / dim1;
+      int j = lower[1] + ((idx / dim3) / dim2) % dim1;
+      int k = lower[2] +  (idx / dim3) % dim2;
+      int l = lower[3] +   idx % dim3;
+      f(i,j,k,l);
+    });
+  } else {
+    EKAT_KERNEL_ERROR_MSG ("Error! Unsupported rank for md_team_vector_range_for.");
+  }
+};
+
+// Team-vector range with lower bound all zeros
+template<typename TeamMember, int N, typename Functor>
+KOKKOS_INLINE_FUNCTION
+void md_team_vector_range_for (const TeamMember& t, int dim[N], const Functor& f)
+{
+  if constexpr (N==1) {
+    Kokkos::parallel_for(Kokkos::TeamVectorRange(t,dim[0]),[&](int i) {
+      f(i);
+    });
+  } else if constexpr (N==2) {
+    Kokkos::parallel_for(Kokkos::TeamVectorRange(t,dim[0]*dim[1]),[&](int idx) {
+      int i = idx / dim[1];
+      int j = idx % dim[1];
+      f(i,j);
+    });
+  } else if constexpr (N==3) {
+    Kokkos::parallel_for(Kokkos::TeamVectorRange(t,dim[0]*dim[1]*dim[2]),[&](int idx) {
+      int i = (idx / dim[2]) / dim[1];
+      int j = (idx / dim[2]) % dim[1];
+      int k =  idx % dim[2];
+      f(i,j,k);
+    });
+  } else if constexpr (N==4) {
+    Kokkos::parallel_for(Kokkos::TeamVectorRange(t,dim[0]*dim[1]*dim[2]*dim[3]),[&](int idx) {
+      int i = ((idx / dim[3]) / dim[2]) / dim[1];
+      int j = ((idx / dim[3]) / dim[2]) % dim[1];
+      int k =  (idx / dim[3]) % dim[2];
+      int l =   idx % dim[3];
+      f(i,j,k,l);
+    });
+  } else {
+    EKAT_KERNEL_ERROR_MSG ("Error! Unsupported rank for md_team_vector_range_for.");
+  }
 };
 
 #endif // EKAT_MD_RANGE_HPP
