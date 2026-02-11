f7643a8914f186c5b269bae0f6cb210f954df1a0
Thomas Clevenger <ac.tccleve@chrlogin1.lcrc.anl.gov>
2026-02-11 12:14:18 -0600

    Add generic cmake

--- cmake/machine-files/chrysalis.cmake
+++ cmake/machine-files/chrysalis.cmake
@@ -1,4 +1,5 @@
 # Load openmp backend for kokkos
+include (${CMAKE_CURRENT_LIST_DIR}/kokkos/generic.cmake)
 include (${CMAKE_CURRENT_LIST_DIR}/kokkos/openmp.cmake)
 
 include (${CMAKE_CURRENT_LIST_DIR}/mpi/srun.cmake)
