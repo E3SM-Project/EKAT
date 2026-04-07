3609e1f63cae59696bbdbc072d1c7e65e8aec4b7
Luca Bertagna <lbertag@sandia.gov>
2026-04-06 22:58:00 -0600

    Use an new tag of Catch2, that allows install even when built as subproject

--- cmake/tpls/EkatBuildCatch2.cmake
+++ cmake/tpls/EkatBuildCatch2.cmake
@@ -6,10 +6,12 @@ message (STATUS "  Fetching Catch2 via FetchContent")
 # Do not allow the user to enable these
 set(CATCH_BUILD_TESTING OFF CACHE BOOL "Disable Catch2 internal tests" FORCE)
 set(CATCH_INSTALL_HELPERS OFF CACHE BOOL "Disable Catch2 install" FORCE)
+set(CATCH_FORCE_INSTALL ON CACHE BOOL "Force install even if Catch2 is a subproject" FORCE)
+set(CATCH_INSTALL_DOCS OFF CACHE BOOL "Install documentation alongside library" FORCE)
 
 FetchContent_Declare(Catch2
   GIT_REPOSITORY https://github.com/E3SM-Project/Catch2.git
-  GIT_TAG        958944d27a2d2fb82aa008377bf4f8752f6b848e
+  GIT_TAG        0d70154ddf178cf99bca140669e0618267e51682
   SOURCE_DIR     ${EKAT_SOURCE_DIR}/extern/Catch2
   BINARY_DIR     ${EKAT_BINARY_DIR}/extern/Catch2
 )
