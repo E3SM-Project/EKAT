2f78b031d00dead250894e481c717ea6a61e99e1
Luca Bertagna <lbertag@sandia.gov>
2026-06-02 21:01:37 -0600

    Do not set test max threads in cacts, but in machine files
    Cacts was using up to num_run_res, which can be ridicolously large

--- cmake/machine-files/ghci-snl-cpu.cmake
+++ cmake/machine-files/ghci-snl-cpu.cmake
@@ -2,4 +2,5 @@
 include(${CMAKE_CURRENT_LIST_DIR}/ghci-snl.cmake)
 
 # Currently, we have 32 cores for each ghci-snl-cpu instance, but 4 ranks is enough
-set(EKAT_TEST_MAX_RANKS 4 CACHE STRING "Upper limit on ranks for mpi tests")
+set(EKAT_TEST_MAX_RANKS 4 CACHE STRING "Upper limit on number of ranks for mpi tests")
+set(EKAT_TEST_MAX_THREADS 8 CACHE STRING "Upper limit on number of threads for mpi tests")
--- cmake/machine-files/ghci-snl-oneapi.cmake
+++ cmake/machine-files/ghci-snl-oneapi.cmake
@@ -5,4 +5,5 @@ include(${CMAKE_CURRENT_LIST_DIR}/ghci-snl.cmake)
 option (Kokkos_ARCH_SPR "" ON)
 
 # Currently, we have 192 cores on blake, but testing 4 ranks is probably enough
-set(EKAT_TEST_MAX_RANKS 4 CACHE STRING "Upper limit on ranks for mpi tests")
+set(EKAT_TEST_MAX_RANKS 4 CACHE STRING "Upper limit on number of ranks for mpi tests")
+set(EKAT_TEST_MAX_THREADS 8 CACHE STRING "Upper limit on number of threads for mpi tests")
